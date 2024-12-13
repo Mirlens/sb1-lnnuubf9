cb0170e6feff8b63f1bd5daf523d4645063a8393
Mirlens <20zamouk02@gmail.com>
2024-12-13 22:09:28 +0100

    pics1

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,31 +1,5 @@
 import React from 'react';
-
-const categories = [
-  {
-    name: 'Beach Portraits',
-    images: [
-      'https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1514090458221-65bb69cf63e6?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1502680390469-be75c86b636f?auto=format&fit=crop&q=80&w=800'
-    ]
-  },
-  {
-    name: 'Sunsets',
-    images: [
-      'https://images.unsplash.com/photo-1501436513145-30f24e19fcc8?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1566241440091-ec10de8db2e1?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&q=80&w=800'
-    ]
-  },
-  {
-    name: 'Events',
-    images: [
-      'https://images.unsplash.com/photo-1519046904884-53103b34b206?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?auto=format&fit=crop&q=80&w=800',
-      'https://images.unsplash.com/photo-1540555700478-4be289fbecef?auto=format&fit=crop&q=80&w=800'
-    ]
-  }
-];
+import { categories } from './categories';
 
 export const Gallery = () => {
   return (
---
+++ src/components/categories.tsx
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    name: 'Beach Portraits',
+    images: [
+      'https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?auto=format&fit=crop&q=80&w=800',
+      'https://images.unsplash.com/photo-1514090458221-65bb69cf63e6?auto=format&fit=crop&q=80&w=800',
+      'https://drive.google.com/file/d/1lwRRs9fnzL2PTJfoN-4fxCt5Q4ASbONK/view?usp=drive_link'
+    ]
+  },
+  {
+    name: 'Sunsets',
+    images: [
+      'https://images.unsplash.com/photo-1501436513145-30f24e19fcc8?auto=format&fit=crop&q=80&w=800',
+      'https://images.unsplash.com/photo-1566241440091-ec10de8db2e1?auto=format&fit=crop&q=80&w=800',
+      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&q=80&w=800'
+    ]
+  },
+  {
+    name: 'Events',
+    images: [
+      'https://images.unsplash.com/photo-1519046904884-53103b34b206?auto=format&fit=crop&q=80&w=800',
+      'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?auto=format&fit=crop&q=80&w=800',
+      'https://images.unsplash.com/photo-1540555700478-4be289fbecef?auto=format&fit=crop&q=80&w=800'
+    ]
+  }
+];
